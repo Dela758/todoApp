7c2269e5c0b7ce02fb1dfeaaa089f1d40947eb7d
Dela758 <del.ocloo@gmail.com>
2022-07-21 14:53:22 +0000

    Added form to capture data and implemented button functions

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # Path for homepage
+    path('', views.index, name='index'),
+    # Path for form
+    path('add', views.addTodoItem, name='add'),
+    # Path for completed
+    path('completed/<todo_id>', views.completedTodo, name='completed'),
+    # Path to delete all completed items
+    path('deletecompleted', views.deleteCompleted, name='deletecompleted'),
+    # Path to delete all items
+    path('deleteAll', views.deleteAll, name='deleteAll')
+
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from todolist.forms import TodoListForm
 
 from .models import Todolist
+
+from .forms import TodoListForm
+
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
     todo_items = Todolist.objects.order_by('id')
-    context = {'todo_items' : todo_items}
+    form = TodoListForm()
+    context = {'todo_items' : todo_items, 'form' : form}
     return render(request, 'todolist/index.html', context)
+
+# View for adding form input to database
+@require_POST
+def addTodoItem(request):
+    form = TodoListForm(request.POST)
+
+    if form.is_valid():
+        new_todo = Todolist(text = request.POST['text'])
+        new_todo.save()
+
+    return redirect('index')
+
+# View for completed items
+def completedTodo(request, todo_id):
+    # Query database for specific todo_id 
+    todo = Todolist.objects.get(pk = todo_id)
+    # Update database by setting value to true
+    todo.completed = True
+    # Save changes
+    todo.save()
+
+    return redirect('index')
+
+# View to delete completed items
+def deleteCompleted(request):
+    # Query database for all items marked as completed and delete
+    Todolist.objects.filter(completed__exact=True).delete()
+
+    return redirect('index')
+
+# View to delete all items
+def deleteAll(request):
+    # Query the database for all items and call the delete method
+    Todolist.objects.all().delete()
+
+    return redirect('index')
